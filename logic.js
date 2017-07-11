1fa271366bf1e1726c44990a16a0cc4811caea52
Damon Oehlman <damon.oehlman@gmail.com>
2017-07-11 19:12:00 +1000

    s/times/repeat/

--- logic.js
+++ logic.js
@@ -11,10 +11,19 @@ import type {
  * Execute an instruction builder <tt>count</tt> times. Any arguments (untyped) that
  * should be supplied to the builder can be passed in the rest parameters.
  */
-function repeat(count: number, builder: InstructionBuilder): NestedInstructionList {
+function repeat(count: number, builder: InstructionBuilder | Array<Instruction> | Instruction): NestedInstructionList {
   let items = [];
   for (let index = 0; index < count; index += 1) {
-    items = items.concat(builder());
+    let nextItems = [];
+    if (typeof builder == 'function') {
+      nextItems = builder();
+    } else if (Array.isArray(builder)) {
+      nextItems = builder;
+    } else {
+      nextItems = [builder];
+    }
+
+    items = items.concat(nextItems);
   }
 
   return {
