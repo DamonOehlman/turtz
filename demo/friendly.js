08fdd9dd4bfcbe5991b893db64e03e201f71df55
Damon Oehlman <damon.oehlman@gmail.com>
2017-07-12 19:56:02 +1000

    typechecking

--- demo/friendly.js
+++ demo/friendly.js
@@ -59,7 +59,9 @@ window.addEventListener('DOMContentLoaded', () => {
      */
 
     // surface.draw(path, 0, 0);
-    surface.draw(square(50), 0, 0);
-    return surface.draw(poly(9, 50), 0, 0);
+    return [
+      surface.draw(square(50), 0, 0),
+      surface.draw(poly(9, 50), 0, 0)
+    ];
   });
 });
